76808c656b04a4dc2bd3978f4421e07b8a0bf468
Agung Saputra <agungsaputradifh@gmail.com>
2024-06-06 08:32:47 +0700

    fix bug ubah judul

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import {BrowserRouter, Router, Route} from 'react-router-dom'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Netflix from './pages/Netflix'
 
 export default function App() {
   return (
-    <BrowserRouter></BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/Signup' element={<Signup/>}/>
+        <Route path='/' element={<Netflix/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Netfix() {
   return (
-    <div>Netfix</div>
+    <div>Netflix</div>
   )
 }
