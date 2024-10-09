b3821caaf70102e03bfbf68728e4ed56f83a10b9
Agung Saputra <agungsaputradifh@gmail.com>
2024-10-09 23:43:40 +0700

    navbar

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar';
 
 export default function Netfix() {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+    
+    window.onscroll = () => {
+      setIsScrolled(window.scrollY === 0 ? false: true);
+      return () => (window.onscroll = null)
+    }
+
   return (
-    <div>Netflix</div>
+    <div>
+      <Navbar isScrolled={isScrolled}/>
+    </div>
   )
 }
