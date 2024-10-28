fded62c300854e91d58e66aab2bfa2eca7a12c3b
Agung Saputra <agungsaputradifh@gmail.com>
2024-10-28 22:53:41 +0700

    card v0.2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,7 +17,7 @@ export default function Card({movieData, isLiked = false}) {
       {
         isHovered && (
           <div className="hover">
-            <div className="image-vide-container">
+            <div className="image-video-container">
               <img 
                 src={`https://image.tmdb.org/t/p/w500${movieData.image}`} 
                 alt="movie" 
@@ -50,10 +50,12 @@ export default function Card({movieData, isLiked = false}) {
                   <BiChevronDown title='More Info'/>
                 </div>
               </div>
-              <div className="genre flex">
-                <ul className='flex'>{movieData.genres.map((genre) => {
-                  <li key={genre}>{genre}</li>
-                })}</ul>
+              <div className="genres flex">
+                <ul className='flex'>
+                  {movieData.genres.map((genre) => {
+                  return <li key={genre}>{genre}</li>
+                  })}
+                </ul>
               </div>
             </div>
           </div>
@@ -62,4 +64,79 @@ export default function Card({movieData, isLiked = false}) {
     </Container>
   );
 };
-const Container = styled.div``;
+const Container = styled.div`
+  max-width: 230px;
+  width: 230px;
+  height: 100%;
+  cursor: pointer;
+  position: relative;
+  img {
+    border-radius: 0.2rem;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+  }
+  .hover {
+    z-index: 90;
+    height: max-content;
+    width: 20rem;
+    position: absolute;
+    top: -18vh;
+    left: 0;
+    border-radius: 0.3rem;
+    box-shadow: rgba(0,0,0,0.75) 0px 3px 10px;
+    background-color: #181818;
+    transition: 0.3s ease-in-out;
+    .image-video-container {
+      position: relative;
+      height: 140px;
+      img {
+        width: 100%;
+        height: 140px;
+        object-fit: cover;
+        border-radius: 0.3rem;
+        top: 0;
+        z-index: 4;
+        position: absolute;
+      }
+      video {
+        width: 100%;
+        height: 140px;
+        object-fit: cover;
+        border-radius: 0.3rem;
+        top: 0;
+        z-index: 5;
+        position: absolute;
+      }
+    }
+    .info-container {
+      padding: 1rem;
+      gap: 0.5rem;
+    }
+    .icons {
+      .controls {
+        display: flex;
+        gap: 1rem;
+      }
+      svg {
+        font-size: 2rem;
+        cursor: pointer;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: #b8b8b8;
+        }
+      }
+    }
+    .genres {
+      ul {
+        gap: 1rem;
+        li {
+          padding-right: 0.7rem;
+          &:first-of-type {
+            list-style-type: none;
+          }
+        }
+      }
+    }
+  }
+`;
